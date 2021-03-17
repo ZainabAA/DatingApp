c1c09eb06cf583389675fb29dccfabd5287bcf2f
zainab <young8femme@gamil.com>
2021-03-17 12:48:26 +0300

    added members to interface

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { error } from '@angular/compiler/src/util';
 import { Component, OnInit } from '@angular/core';
-import { User } from './models/user';
+import { User } from './_models/user';
 import { AccountService } from './_services/account.service';
 
 @Component({
